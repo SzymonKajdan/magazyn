0c3781c09d6ad3c1cb300d8e3549c0a2662eab3e
Marcin Witkowski <witkowskimarcin@hotmail.com>
2019-03-16 13:16:58 +0100

    Poprawa bledu w LocationRepository

--- src/main/java/com/example/repository/LocationRepository.java
+++ src/main/java/com/example/repository/LocationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
-    Location findByBarCodeLocation();
+    Location findByBarCodeLocation(String barCodeLocation);
 }
